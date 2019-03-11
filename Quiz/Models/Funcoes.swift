b07a868bda3c44033cf6f006c287fcd16f749523
Rafael <jonatas.luz.91@gmail.com>
2019-03-10 21:48:23 -0300

    Primeiro commit

---
+++ Quiz/Models/Funcoes.swift
@@ -0,0 +1,11 @@
+//
+//  Funcoes.swift
+//  Quiz
+//
+//  Created by Rafael Avelino do Prado Garcia Ribeiro on 10/03/19.
+//  Copyright © 2019 Guest User. All rights reserved.
+//
+
+import Foundation
+
+
--- Quiz/PerguntaViewController.swift
+++ Quiz/PerguntaViewController.swift
@@ -22,15 +22,15 @@ class PerguntaViewController: UIViewController {
     var respostasTeste:[(resposta : String, indice : Int )] = [(resposta : "Leonard Nimoy",indice : 0),(resposta : "Willian Shatner", indice : 1), (resposta :"Nichelle Nichols", indice : 2),(resposta: "Jean-Luc Picard", indice:0), (resposta:"James T. Kirk", indice:1),(resposta:"Christopher Pike", indice:2),(resposta:"Humano e Vulcan", indice:0),(resposta:"Humano e Klingon", indice:1),(resposta:"Ferengi e Betazoide", indice:2)]
     
     var qtdAcertos : Int = 0
-    var questaoAtual : Int = 0
-    
-    
+
+     var questaoAtual : Int = 1
+
     override func viewDidLoad() {
         super.viewDidLoad()
         var contador : Int = 0
-        for a in 0 ... 2{
+        for carregaArrayPergunta in 0 ... 2{
             let questao = Pergunta()
-            questao.setPergunta(perguntasTeste[a])
+            questao.setPergunta(perguntasTeste[carregaArrayPergunta])
             for _ in 0 ... 2{
                 questao.setResposta(resp: respostasTeste[contador])
                 contador += 1
@@ -39,20 +39,32 @@ class PerguntaViewController: UIViewController {
             
         }
 
+        
+       
+        
+        
         pergunta.text=(questoes[0].questao)
         resposta1.setTitle(questoes[0].respostas[0].resposta, for:[])
-        resposta2.setTitle(respostasTeste[1].resposta, for: [])
-        resposta3.setTitle(respostasTeste[2].resposta, for: [])    
+        resposta2.setTitle(questoes[0].respostas[1].resposta, for: [])
+        resposta3.setTitle(questoes[0].respostas[2].resposta, for: [])
+        
         // Do any additional setup after loading the view.
     }
     
+
     @IBAction func resposta1Action(_ sender: UIButton) {
-        if respostasTeste[0].indice == 0{
+        if questaoAtual != questoes.count{
+            if respostasTeste[questaoAtual].indice == 0{
+                
+                qtdAcertos += 1
+            }
+            pergunta.text=(questoes[questaoAtual].questao)
+            resposta1.setTitle(questoes[questaoAtual].respostas[0].resposta, for:[])
+            resposta2.setTitle(questoes[questaoAtual].respostas[1].resposta, for: [])
+            resposta3.setTitle(questoes[questaoAtual].respostas[2].resposta, for: [])
+            questaoAtual += 1
             
-            qtdAcertos += 1
         }
-        sleep(1)
-        resposta1.setTitle(respostasTeste[3].resposta, for: [])
     }
     
     
